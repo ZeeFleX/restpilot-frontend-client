40b76cbbc37021218fca746c201c96839db18152
Ilya Kardapolov <kardapolov@gmail.com>
2024-12-09 20:17:44 +0500

    Gitignore for components

--- src/lib/components/menuCategories/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as MenuCategories } from './menuCategories.svelte';
-export * from './menuCategories.types';
--- src/lib/components/menuItem/index.ts
+++
@@ -1,4 +0,0 @@
-import MenuItem from './menuItem.svelte';
-
-export { default as MenuItem } from './menuItem.svelte';
-export * from './menuItem.types';
